9029ad71f286b55a4543a2296e92ff437a48e73f
analogy47 <actweed47@gmail.com>
2019-05-05 04:59:43 -0400

    Fixing import errors

--- hairspray-app/src/components/LandPage.js
+++ hairspray-app/src/components/LandPage.js
@@ -1,49 +1,48 @@
-import React, { Component } from "react";
+import React, { Component } from 'react'
 // import { Button, Grid, Row, Col, Image } from "react-bootstrap";
-import Header from "./Header";
-import { Link } from "react-router-dom";
-import { getAllStylists } from "../actions";
-import { connect } from "react-redux";
+import Header from './Header'
+import { Link } from 'react-router-dom'
+import { getAllStylists } from '../actions'
+import { connect } from 'react-redux'
 
-import { UncontrolledCarousel, Button } from "reactstrap";
+import { UncontrolledCarousel, Button } from 'reactstrap'
 
 class LandPage extends Component {
-  constructor(props) {
-    super(props);
-    super(props);
-    this.stylists = [];
+  constructor (props) {
+    super(props)
+    this.stylists = []
   }
 
-  componentDidMount() {
-    this.props.getAllStylists();
+  componentDidMount () {
+    this.props.getAllStylists()
   }
 
-  render() {
+  render () {
     if (this.props.gettingStylists) {
     } else {
       this.props.stylists.forEach((stylist, i) => {
         this.stylists.push({
           src: stylist.image
-        });
-      });
+        })
+      })
     }
     return (
-      <div className="landing">
-        <div className="landing__nav">
+      <div className='landing'>
+        <div className='landing__nav'>
           <Header />
         </div>
         {this.props.gettingStylists ? null : (
           <UncontrolledCarousel
-            className="landing__carousel"
+            className='landing__carousel'
             items={this.stylists}
           />
         )}
         Book an appointment now, with one of these amazing stylists!
-        <Link to="/signin">
-          <Button className="landing__button">Schedule Now!</Button>
+        <Link to='/signin'>
+          <Button className='landing__button'>Schedule Now!</Button>
         </Link>
       </div>
-    );
+    )
   }
 }
 
@@ -51,12 +50,12 @@ const mapStateToProps = state => {
   return {
     gettingStylists: state.stylist.gettingStylists,
     stylists: state.stylist.stylists
-  };
-};
+  }
+}
 
 export default connect(
   mapStateToProps,
   {
     getAllStylists
   }
-)(LandPage);
+)(LandPage)
