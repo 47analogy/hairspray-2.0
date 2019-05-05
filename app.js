0446bb89bc8bda8c6ba0a701c7d8890c96da132c
analogy47 <actweed47@gmail.com>
2019-05-05 03:02:08 -0400

    Set up for deployment

--- app.js
+++ app.js
@@ -1,53 +1,57 @@
-require("dotenv").config();
-const express = require("express");
-const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
-const path = require("path");
-const cookieParser = require("cookie-parser");
-const passport = require("passport");
+require('dotenv').config()
+const express = require('express')
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+const path = require('path')
+const cookieParser = require('cookie-parser')
+const passport = require('passport')
+const config = require('./config')
 
-const CORS = require("cors");
+const CORS = require('cors')
 
-const app = express();
-const port = process.env.PORT || 5000;
-const db = process.env.MONGODB_URI || "mongodb://localhost:27017/hairspray";
-const routes = require("./routes/routes");
+const app = express()
+const port = process.env.PORT || 5000
+const db = process.env.MONGODB_URI || 'mongodb://localhost:27017/hairspray'
+const routes = require('./routes/routes')
 
-app.use(express.static(path.join(__dirname, "../hairspray-app/build")));
+app.use(express.static(path.join(__dirname, '../hairspray-app/build')))
 
 // connect to database
 mongoose
   .connect(db)
-  .then(() => console.log("MongoDB is connected"))
-  .catch(err => console.log(err));
+  .then(() => console.log('MongoDB is connected'))
+  .catch(err => console.log(err))
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-app.use(CORS());
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.use(CORS())
 
 // const REACT_WHITELIST = require("../hairspray-app/src/components/StripeCheckout");
 
 const whitelist = [
-  "http://localhost:5000",
+  'http://localhost:5000',
   // REACT_WHITELIST,
-  "https://obscure-island-58835.herokuapp.com/"
-];
+  'https://obscure-island-58835.herokuapp.com/'
+]
 
 app.use(
   CORS({
     origin: whitelist,
     credentials: true,
-    methods: ["GET", "PUT", "POST", "DELETE"]
+    methods: ['GET', 'PUT', 'POST', 'DELETE']
   })
-);
-app.options("*", CORS());
+)
+app.options('*', CORS())
 
-routes(app);
+routes(app)
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname+'/hairspray-app/build/index.html'));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/hairspray-app/build/index.html'))
   // res.sendFile(path.join(`${__dirname  }/hairspray-app/build/index.html`));
+})
 
-});
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
+})
 
-app.listen(port, () => console.log(`app running on port ${port}`));
+app.listen(port, () => console.log(`app running on port ${port}`))
--- config/settings.js
+++ config/settings.js
@@ -1,3 +1,5 @@
-module.exports = {
-  secret: "isthisreallysecure?"
-};
+const config = {}
+
+config.secret = process.env.SECRET || 'isthisreallysecure?'
+
+module.exports = config
